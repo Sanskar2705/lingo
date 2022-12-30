f0d7820bc763f6243e97c2b80b1b339596a767a6
Sanskar2705 <117569887+Sanskar2705@users.noreply.github.com>
2022-12-30 10:07:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ current_card ={}
 to_learn ={}
 
 try:
-    data= pandas.read_csv("data/words_to_learn.csv")
+    data= pandas.read_csv("words_to_learn.csv")
 except FileNotFoundError:
-    original_data= pandas.read_csv("data/farsi.csv")
+    original_data= pandas.read_csv("farsi.csv")
     to_learn= original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
@@ -44,8 +44,8 @@ window.config(pady=50, padx=50, bg= BACKGROUND_COLOR)
 flip_timer= window.after(4000, func= flip_card)
 
 canvas =Canvas(width=800, height=526)
-card_front_img =PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
+card_front_img =PhotoImage(file="card_front.png")
+card_back_img = PhotoImage(file="card_back.png")
 
 card_background= canvas.create_image(400, 263 , image= card_front_img)
 card_title= canvas.create_text(400, 150, text="Title", font=("Ariel", 30, "italic"))
@@ -53,11 +53,11 @@ card_word= canvas.create_text(400,263, text="Word", font=("Ariel", 45, "bold"))
 canvas.grid(row=0,column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
-cross_image= PhotoImage(file="images/wrong.png")
+cross_image= PhotoImage(file="wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command= next_card)
 unknown_button.grid(row=1, column=0)
 
-check_image = PhotoImage(file= "images/right.png")
+check_image = PhotoImage(file= "right.png")
 known_button =Button(image=check_image, highlightthickness=0, command= is_known)
 known_button.grid(row=1,column=1)
 
